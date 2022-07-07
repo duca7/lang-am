343f5f5298fe5866f45299c4ae914208760cde68
duca7 <hohuynhduca7bh123@gmail.com>
2022-07-07 21:44:16 +0700

    roi-nuoc page

---
+++ components/WaterPuppet.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: 'default'
+    },
+    description: {
+      type: String,
+      default: 'default'
+    },
+    background: {
+      type: String,
+      default: 'default'
+    }
+  }
+};
+</script>
---
+++ pages/roi-nuoc/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="">
+    <AppTitle :level="3">
+      Trò chơi
+    </AppTitle>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss">
+  .background {
+    width: 100%;
+  }
+</style>
