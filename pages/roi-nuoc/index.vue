1d7f3d90e535f33bea24b074dbd0cf216ed3da3c
duca7 <hohuynhduca7bh123@gmail.com>
2022-07-09 22:33:49 +0700

    fix button bottom

--- pages/roi-nuoc/index.vue
+++ pages/roi-nuoc/index.vue
@@ -14,7 +14,13 @@
     />
 
     <WatterPuppetActivity />
-    <AppButtonBottomRight />
+    <div>
+      <AppButtonRed class="btn-bottom feedback">
+        <img src="icons/gio.svg" alt="cai-gio-bat-ca">
+        Đặt vé
+      </AppButtonRed>
+    </div>
+
     <div class="overlay" />
   </main>
 </template>
@@ -55,6 +61,29 @@ export default {
     padding: 0 1rem;
   }
 
+  .btn-bottom {
+    position: fixed;
+    bottom: 0px;
+    right: 0px;
+    z-index: 1;
+}
+
+.feedback {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: $primary-dark-red-100;
+    color: white;
+    width: 170px;
+    height: 55px;
+    border-top-left-radius: 20px;
+    border: none;
+    font-size: 1.25rem;
+    font-weight: 400;
+    img {
+        padding-right: 1rem;
+    }
+}
   .overlay {
     @include overlay("images/la-sen-lon.png");
   }
