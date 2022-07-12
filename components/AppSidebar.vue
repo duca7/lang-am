3995b94fc994057511e44a7ad098d28cf85d88e8
duca7 <hohuynhduca7bh123@gmail.com>
2022-07-12 20:18:51 +0700

    fix layout

--- components/AppSidebar.vue
+++ components/AppSidebar.vue
@@ -91,7 +91,8 @@ export default {
         path: '/tro-tich'
       },
       {
-        displayText: 'Rồng Vàng'
+        displayText: 'Rồng Vàng',
+        path: 'rong-vang'
       },
       {
         displayText: 'Booking'
---
+++ pages/rong-vang/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <AppTitle :level="1" title="Rồng Vàng">
+      Nhà hát múa rối nước tư nhân đầu tiên của Việt Nam mang tên Rồng Vàng.
+    </AppTitle>
+
+    <WaterPuppetArticle
+      title="Nhà hát Rồng Vàng"
+      description="Địa điểm biểu diễn những tiết mục múa rối nước dân gian Việt Nam duy nhất ở Tp. Hồ Chí Minh được diễn ra trên sân khấu nước."
+      background="/images/bat-tien-bg.png"
+    />
+
+    <div>
+      <AppButtonRed class="btn-bottom feedback">
+        <img src="icons/gio.svg" alt="cai-gio-bat-ca">
+        Đặt vé
+      </AppButtonRed>
+    </div>
+  </div>
+</template>
