703dcc5b5c66d98799ed6848837aff0865cb2bd3
ankuanua123@gmail.com <ankuanua123@gmail.com>
2022-07-13 09:09:46 +0700

    feature: add show/extra-infomation

---
+++ components/app/Input.vue
@@ -0,0 +1,24 @@
+<template>
+  <input class="app-input font-rlw">
+</template>
+
+<style lang="scss" scoped>
+.app-input {
+  $input-color:#797979;
+
+  margin-top: 20px;
+  background: none;
+  width: 100%;
+  padding-top: 0.5em 0 0.3em;
+  font-weight: 600;
+  outline: 0;
+  border: none;
+  font-size: 1.1rem;
+  border-bottom: 1px solid $input-color;
+  color: $input-color;
+
+  :focus {
+    border-color: #fff;
+  }
+}
+</style>
